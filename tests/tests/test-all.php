6ecca337c0be682e9559534af606aaa04e111e18
Zack Tollman <tollmanz@gmail.com>
2014-07-05 11:39:57 -0700

    Only add memcached servers if not defined already.

--- tests/tests/test-all.php
+++ tests/tests/test-all.php
@@ -10,9 +10,13 @@ class MemcachedUnitTests extends WP_UnitTestCase {
 	public function setUp() {
 		parent::setUp();
 		global $memcached_servers;
-		$memcached_servers = array(
-			'default' => array( '127.0.0.1', 11211 ),
-		);
+
+		if ( ! is_array( $memcached_servers ) ) {
+			$memcached_servers = array(
+				'default' => array( '127.0.0.1', 11211 ),
+			);
+		}
+
 		$this->object_cache = new WP_Object_Cache();
 		$this->object_cache->flush();
 		$this->servers = $this->object_cache->servers;
