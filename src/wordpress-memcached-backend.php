bc89606d2604d5452abcde160c407a6bc684cf17
Zack Tollman <tollmanz@gmail.com>
2014-07-26 09:48:17 -0700

    Add main plugin file.

---
+++ src/wordpress-memcached-backend.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name:    WordPress Memcached Backend
+ * Plugin URI:     https://github.com/tollmanz/wordpress-memcached-backend
+ * Description:    Object cache drop using the Memcached PECL extension to interface with Memcached.
+ * Version:        1.0.0.
+ * Author:         Zack Tollman
+ * Author URI:     http://tollmanz.com
+ * License:        GPLv2 or later
+ * License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+ */
