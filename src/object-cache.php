25879d60041bb04e4cf64423aae4175c7118858a
Zack Tollman <tollmanz@gmail.com>
2014-11-28 16:35:14 -0800

    Add correct plugin headers.
    Updated the drop-in plugin headers to match that of the plugin headers.
    I originally accepted a pull request without realizing I had the real
    headers set up. I'm using the ones I've previously used as they are the
    best option for this plugin.

--- src/object-cache.php
+++ src/object-cache.php
@@ -1,12 +1,14 @@
 <?php
-/*
-Plugin Name: WordPress pecl memcached object cache
-Description: The real Memcached (not Memcache) backend for the WP Object Cache.
-Version:
-Plugin URI: https://github.com/tollmanz/wordpress-pecl-memcached-object-cache
-Author: Zack Tollman
-Author URI: http://tollmanz.com/
-*/
+/**
+ * Plugin Name:    PECL Memcached Object Cache
+ * Plugin URI:     https://github.com/tollmanz/wordpress-memcached-backend
+ * Description:    Object cache drop-in using the Memcached PECL extension to interface with Memcached.
+ * Version:        1.0.0.
+ * Author:         Zack Tollman
+ * Author URI:     http://tollmanz.com
+ * License:        GPLv2 or later
+ * License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 /**
  * Adds a value to cache.
