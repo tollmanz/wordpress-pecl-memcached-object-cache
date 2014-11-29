67245f06057617192b72d6c44f24ba4545829253
Zack Tollman <tollmanz@gmail.com>
2014-11-28 16:40:32 -0800

    Remove plugin headers from drop-in.
    Since the plugin will ultimately be installed as a plugin with the
    drop-in symlinked from the plugins directory, having two header blocks
    makes WP think there are two different plugins within the one package.
    The headers need to be removed for now.

--- src/object-cache.php
+++ src/object-cache.php
@@ -1,16 +1,5 @@
 <?php
 /**
- * Plugin Name:    PECL Memcached Object Cache
- * Plugin URI:     https://github.com/tollmanz/wordpress-memcached-backend
- * Description:    Object cache drop-in using the Memcached PECL extension to interface with Memcached.
- * Version:        1.0.0.
- * Author:         Zack Tollman
- * Author URI:     http://tollmanz.com
- * License:        GPLv2 or later
- * License URI:    http://www.gnu.org/licenses/gpl-2.0.html
- */
-
-/**
  * Adds a value to cache.
  *
  * If the specified key already exists, the value is not stored and the function
