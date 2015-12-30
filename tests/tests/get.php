2fc1e99b9877f0bd4f10c98b2a4d3ea1a511fe49
Alex <ulkoalex@gmail.com>
2015-12-30 05:42:43 +0200

    Travis debug

--- tests/tests/get.php
+++ tests/tests/get.php
@@ -251,10 +251,13 @@ class MemcachedUnitTestsGet extends MemcachedUnitTests {
 		$key = microtime().rand(0, getrandmax());
 
 		// Return false with value not yet set
-		$this->assertFalse( $this->object_cache->get( $key, 'default', false, $found, '', false, null, $cas_token ) );
+		$value = $this->object_cache->get( $key, 'default', false, $found, '', false, null, $cas_token );
+		$this->assertFalse( $value );
 
 		// Verify that we have a CAS token
 		$this->assertTrue( is_null( $cas_token ) );
+		$this->assertEquals(null, $cas_token);
+		$this->assertEquals(false, $value);
 	}
 
 	public function test_get_value_with_cas_token_and_callback() {
@@ -559,10 +562,13 @@ class MemcachedUnitTestsGet extends MemcachedUnitTests {
 		$server_key = microtime().rand(0, getrandmax());
 
 		// Return false with value not yet set
-		$this->assertFalse( $this->object_cache->getByKey( $server_key, $key, 'default', false, $found, null, $cas_token ) );
+		$value = $this->object_cache->getByKey( $server_key, $key, 'default', false, $found, null, $cas_token );
+		$this->assertFalse( $value );
 
 		// Verify that we have a CAS token
 		$this->assertTrue( is_null( $cas_token ) );
+		$this->assertEquals(null, $cas_token);
+		$this->assertEquals(false, $value);
 	}
 
 	public function test_get_by_key_value_with_cas_token_and_callback() {
