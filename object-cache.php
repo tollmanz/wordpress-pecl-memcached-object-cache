556a3ebba34ed6819b1f3c1dc41b3d7c3016e4d2
Joe Hoyle <joehoyle@gmail.com>
2017-03-17 15:06:15 +0800

    Re-introduce PHP notice.

--- object-cache.php
+++ object-cache.php
@@ -279,8 +279,7 @@ function wp_cache_fetch_all() {
  * @return bool             Returns TRUE on success or FALSE on failure.
  */
 function wp_cache_flush( $delay = 0 ) {
-	$caller = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 1 );
-	$caller = $caller[0];
+	$caller = array_shift( debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 1 ) );
 	if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
 		trigger_error( sprintf( 'wp_cache_flush() is only allowed via WP CLI. Called in %s line %d', $caller['file'], $caller['line'] ), E_USER_WARNING );
 		return false;
